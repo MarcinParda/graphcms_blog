7aabc312989e92de2250f5313985671a08218faf
MarcinParda <marcin98parda@gmail.com>
2021-12-04 22:11:05 +0100

    Categories and Post Widgets

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { getCategories } from "/services";
 
-const categories = [{name: 'React', slug: 'react'}, {name: 'Web Development', slug: 'web-dev'}]
+const Header = ( ) => {
 
-const Header = ({ post }) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories()
+      .then((newCategories) => setCategories(newCategories))
+  }, []);
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
