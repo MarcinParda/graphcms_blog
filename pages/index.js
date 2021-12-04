c6230c133572e6bdce48cee68cef16fce6da6b6e
MarcinParda <marcin98parda@gmail.com>
2021-12-04 16:57:29 +0100

    fetching post data from graphcms

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,9 @@ import Head from 'next/head'
 import PostCard from "/components/PostCard";
 import PostWidget from "/components/PostWidget";
 import Categories from "/components/Categories";
+import { getPosts } from "/services";
 
-const post = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' }
-]
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -17,7 +13,7 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {post.map((post, index) => (<PostCard post={post} key={post.title} />))}
+          {posts.map((post) => (<PostCard post={post} key={post.title} />))}
         </div>
 
         <div className="lg:col-span-4 col-span-1">
@@ -29,4 +25,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
