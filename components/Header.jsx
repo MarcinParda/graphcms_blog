89a0ae5e62b75203943e6f1c66a18be174a777a1
MarcinParda <marcin98parda@gmail.com>
2021-12-07 20:09:42 +0100

    new background and logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { getCategories } from "/services";
+import Image from "next/image";
+import face from '/public/face.png';
 
 const Header = ( ) => {
 
@@ -16,9 +18,14 @@ const Header = ( ) => {
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              GraphCMS
-            </span>
+            <div className="cursor-pointer flex align-middle">
+              <Image
+                width={50}
+                height={50}
+                src={face}
+              />
+              <span className="font-bold text-4xl text-white ml-2 mt-2">Programaniak</span>
+            </div>
           </Link>
         </div>
         <div className="hidden md: float-left md:contents">
