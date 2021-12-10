1f43b9c75e1a3c482c8ee42bccfec29bbf8ce457
MarcinParda <marcin98parda@gmail.com>
2021-12-10 23:07:11 +0100

    add dark mode to categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -13,13 +13,13 @@ const Categories = () => {
   
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+    <div className="bg-white dark:bg-gray-900 shadow-lg rounded-lg p-8 mb-8 pb-12">
+      <h3 className="text-xl mb-8 dark:text-gray-100 font-semibold border-b pb-4">
         Categories
       </h3>
       {categories.map((category) => (
         <Link key={category.slug} href={`/category/${category.slug}`}>
-          <span className="cursor-pointer block pb-3 mb-3">
+          <span className="cursor-pointer block pb-3 mb-3 dark:text-gray-100">
             {category.name}
           </span>
         </Link>
