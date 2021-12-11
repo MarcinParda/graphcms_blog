87356125e89afa1a6d004184ad5f1980a3a23c30
MarcinParda <marcin98parda@gmail.com>
2021-12-11 19:05:20 +0100

    add warning block

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': {
+          400: '#ff9c3e',
+          500: '#FF8000'
+        },
+      }
+    },
   },
   variants: {
     extend: {},
